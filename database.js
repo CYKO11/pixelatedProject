df0d21a80b1acc56ded1f5dec363dcc64b5d2cbc
chadDowman <Chadwddk@gmail.com>
2021-02-11 16:28:36 +0200

    Changes

---
+++ database.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection ({
+    host: process.env.HOST,
+    user: process.env.DBUSER,
+    password: process.env.PASS
+});
+
+con.connect(function(err){
+    if(err){
+        throw err;
+    }else{
+        console.log("Connected Successfully");
+    }
+
+    con.query("CREATE DATABASE mydb", function(err, result){
+        if(err){
+            throw err;
+        }else{
+            console.log("DATABASE CREATED SUCCESSFULLY")
+            console.log(result);
+        }
+    });
+});
+
